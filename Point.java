38c2f7725c11d7590399f48ae3c368f72dbf6cd9
panjinbao <494654334@qq.com>
2023-12-18 13:57:23 +0800

    Point类

--- Point.java
+++ Point.java
@@ -1,2 +1,20 @@
-public class Point {
+class Point {
+    private double x;
+    private double y;
+
+
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+
+    public double getY() {
+        return y;
+    }
 }
